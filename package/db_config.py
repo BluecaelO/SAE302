3be5738281289761f337dd949e01e833c29d0c98
Ludovic ARHIMAN <118427460+LudovicARHIMAN@users.noreply.github.com>
2023-10-23 20:42:35 +0400

    Add comment

--- package/db_config.py
+++ package/db_config.py
@@ -8,6 +8,7 @@ conn=None
 def connexion_db(user,pwd):
     global conn
 
+    # Nous créons ici un dictionnaire qui va se charger de stocker les informations de connexion à la base de données postgres SQL
     db_config = { 
     'dbname': 'postgres',
     'user': user,
@@ -16,10 +17,12 @@ def connexion_db(user,pwd):
     'port': '5432'
     }
     try:
+        # On essaie de se connecter et on print dans le terminal "Connexion réussie" si on arrive à se connecter
         conn = psycopg2.connect(**db_config) 
         print("Connexion réussie")
         return True
     except psycopg2.OperationalError as e:
+        # En cas d'échec de connexion à la db on affiche sur la page le message "Unable to connect to the DB"
         flash('Unable to connect to the DB!',format(e))
         print("Erreur de connexion : " + str(e))
         return False
@@ -42,7 +45,7 @@ def creat_user(user,password):
             CONNECTION LIMIT -1
             PASSWORD '{password}';
 
-            
+            # On crée ici la table qui va servir à contenir les mots de passe des utilisateurs 
             CREATE TABLE IF NOT EXISTS {user+"_vault"} (
                 pass_name VARCHAR(255) PRIMARY KEY,
                 login VARCHAR(255),
@@ -93,6 +96,8 @@ def get_pwd_list():
     # Vérifier si l'utilisateur est connecté
     if user:
         try:
+            # permet de récupérer toutes les données dans le coffre personnel de l'utilisateur. 
+            # Cette requête est utilisée notamment dans la grande majorité des pages lorsque l'on souhaite récupérer des données liées aux mots de passe
             cursor = conn.cursor()
             query = f"SELECT pass_name, site, favorite FROM {user + '_vault'}"
 
@@ -119,7 +124,7 @@ def add_password_to_db(pass_name, password, login, site, fav):
         try:
             cursor = conn.cursor()
             
-            # Vérifier si le mot de passe existe déjà dans la base de données
+            # Vérifier si le mot de passe existe déjà dans la base de données (utiliser dans la page add_pasword)
             query = f"SELECT pass_name FROM public.{user + '_vault'} WHERE pass_name = '{pass_name}'"
             cursor.execute(query)
             existing_password = cursor.fetchone()
@@ -149,6 +154,7 @@ def get_pwd_list_search(value):
     # Vérifier si l'utilisateur est connecté
     if user:
         try:
+            # Cette requête permet de récupérer les infos relatives au mot de passe que l'on recherche à partir de "value" qui provient de ce que l'on entre dans la barre de recherche 
             cursor = conn.cursor()
             query = f"SELECT pass_name, site, favorite FROM {user + '_vault'} WHERE pass_name LIKE '%{value}%'"
 
@@ -174,6 +180,7 @@ def get_pwd_list_search_fav():
     if user:
         try:
             cursor = conn.cursor()
+            # Cette requête recherche les mots de passe favoris en regardant si l'attribue "favorite" vaut 1
             query = f"SELECT pass_name, site, favorite FROM {user + '_vault'} WHERE favorite = '1'"
 
             cursor.execute(query)
@@ -256,6 +263,7 @@ def get_password(value):
     if user:
         try:
             cursor = conn.cursor()
+            # Ici on récupère le mot de passe de l'utilisateur à partir du nom du mot de passe
             query = f"SELECT pass_name, login,password,site,favorite FROM {user + '_vault'} WHERE pass_name = '{value}'"
 
             cursor.execute(query)
@@ -282,7 +290,7 @@ def edit_password_to_db(new_pass_name, new_login, new_password, new_site,new_fav
     if user:
         try:
             cursor = conn.cursor()
-
+            # Ici on modifie les informations d'un mot de passe (pour la page edit password)
             query = f"UPDATE public.{user + '_vault'} SET pass_name = '{new_pass_name}', login = '{new_login}', password = '{new_password}', site = '{new_site}', favorite = '{new_favorite}' WHERE pass_name = '{pass_name}'"
             cursor.execute(query)
             conn.commit()
@@ -304,7 +312,7 @@ def del_password_into_db(pass_name):
     if user:
         try:
             cursor = conn.cursor()
-
+            # Cette requête est utilisée pour le bouton de la page index afin de supprimer le mot de passe sélectionné ici on retrouve le mot de passe avec son nom et on le supprime
             query = f"DELETE FROM public.{user + '_vault'} WHERE pass_name = '{pass_name}';"
             cursor.execute(query)
             conn.commit()
