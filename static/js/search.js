c72b63aa9bbaef04565613a885d96f81cfd12564
Ludovic ARHIMAN <118427460+LudovicARHIMAN@users.noreply.github.com>
2023-10-23 21:34:09 +0400

    Update search.js

--- static/js/search.js
+++ static/js/search.js
@@ -12,12 +12,13 @@ function direction(value) {
         url: "search?value=" + encodeURIComponent(value),
         success: function(data) {
             // Traitement des données récupérées
-            // Remplacement du contenu de l'élément "table-container" avec les données du tableau
+            // Remplacement du contenu de l'élément "password_table" avec les données du tableau
             $("#password_table").html(data);
         }
     });
 }
 
+// On fait la même chose que pour le search mais en donnant une valeur "fav=1" pour les mots de passe en favoris car dans le 
 function search_fav() {
     // Envoi de la requête AJAX au serveur
     $.ajax({
@@ -25,7 +26,7 @@ function search_fav() {
         url: "search?fav=1",
         success: function(data) {
             // Traitement des données récupérées
-            // Remplacement du contenu de l'élément "table-container" avec les données du tableau
+            // Remplacement du contenu de l'élément "password_table" avec les données du tableau
             $("#password_table").html(data);
         }
     });
