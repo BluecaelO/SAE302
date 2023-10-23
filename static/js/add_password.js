92fc40a4beeeadb914f01f602b06e2f425851354
Ludovic ARHIMAN <118427460+LudovicARHIMAN@users.noreply.github.com>
2023-10-23 20:58:10 +0400

    Update add_password.js

--- static/js/add_password.js
+++ static/js/add_password.js
@@ -1,4 +1,5 @@
 function generate_password() {
+    // on  récuprer les ID des différent boutons de la page qui font changer les paramètres de la fonction de génération de mot de passe. On récupére aussi longeur du mot de passe en fonction de valeur de l'input sur la page
     var length = document.getElementById("length").value;
     var upercases = document.getElementById("add_upercases");
     var lowercases = document.getElementById("add_lowercases");
@@ -7,6 +8,7 @@ function generate_password() {
 
     var charset = "";
 
+    // On va ensuite définir la variable charset en fonction des paramètres que l'on a coché pour définir l'ensemble que l'on utilise pour générer les mots de passe
     if (upercases.checked) {
         charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
@@ -19,10 +21,13 @@ function generate_password() {
     if (specials.checked) {
         charset += "!@#$%^&*()";
     }
-
+    
+    // Génération du mot de passe en bouclant sur la longueur souhaitée.
     var retVal = "";
+    
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
+        // Sélection aléatoire d'un caractère dans la chaîne "charset" et ajout à "retVal".
     }
     console.log(retVal);
     document.getElementById("password").value = retVal;
@@ -31,6 +36,7 @@ function generate_password() {
 
 
 function copyPassword() {
+    // cette fonction permet de copier les mots de passe que l'on génère
     var copyText = document.getElementById("password");
     copyText.select();
     document.execCommand("copy");
@@ -42,7 +48,9 @@ function copyPassword() {
     passwordToggle.addEventListener('change', function() {
       const password = document.querySelector('.js-password');
       const passwordLabel = document.querySelector('.js-password-label');
-  
+    
+        // elle permet aussi de pouvoir afficher le mot de passe en lettres ou en points lorsque l'on clique sur l'icône d'oeil
+        // on utilise aussi cette fonction pour changer l'icône d'œil l'on affiche les mots de passe 
       if (password.type === 'password') {
         password.type = 'text';
         passwordLabel.innerHTML = '<i class="fa-solid fa-eye-slash"></i>';
