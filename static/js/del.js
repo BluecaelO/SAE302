35cbfdf44272183922d6b71a155de55467004a0a
Ludovic ARHIMAN <118427460+LudovicARHIMAN@users.noreply.github.com>
2023-10-23 21:07:31 +0400

    Update del.js

--- static/js/del.js
+++ static/js/del.js
@@ -2,10 +2,11 @@ function del_password(value) {
     if (confirm("Are you sure you want to delete this password ?")) {
       $.ajax({
         type: "POST",
-        url: "/del_password?pass_name=" + value,
+        url: "/del_password?pass_name=" + value, // on défini l'url où envoyer les données  pour traitemment (ici suppression)
         success: function(data) {
             $("#password_table").html(data);
         }
       });
     }
   }
+// Ici on utilsie de l'ajax pour changer de façon dynamique le contenu d'un div
